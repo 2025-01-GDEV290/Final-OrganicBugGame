2cda3bfc8a9028542c4a697a2c0448c4669c7f0d
Gabriel-2003 <113034401+Gabriel-2003@users.noreply.github.com>
2025-04-16 19:37:14 -0400

    pressing e does nothing

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -5,39 +5,117 @@ using TMPro;
 
 public class Dialogue : MonoBehaviour
 {
-
     public TMP_Text dialogue;
-    public GameObject dialogue_box; 
-    // Start is called before the first frame update
-    void Start()
+    public GameObject dialogue_box;
+    public GameObject Player; // <-- manually assign this in Inspector
+
+    private bool playerInRange = false;
+    private bool dialogueActive = false;
+    private string currentDialogue = "";
+
+    private Movement playerMovement;
+
+    private void Start()
     {
-        
+        if (Player != null)
+        {
+            playerMovement = Player.GetComponent<Movement>();
+            Debug.Log("Player movement component found.");
+        }
+        else
+        {
+            Debug.LogWarning("Player reference not assigned in the inspector!");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-    }
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        switch (other.tag){
-            case "Milkman":
-                dialogue.text = "I am the milkman. MORE TEXT. MORE TEXT. There is so much text in here to test the text boxes so there will be LOTS OF TEXT BIIIIIG TEXT yes so much text so much text yes yes yes yes yes yes yes I am the milk man";
-                dialogue_box.SetActive(true);
-                break;
-            
-            case "Cow":
-                dialogue.text= "Moo! (You can't milk the cow without a bucket)";
-                dialogue_box.SetActive(true);
-                break;
-                
-            default:
-                dialogue.text = "";
-                break;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E key pressed.");
         }
 
+        if (playerInRange)
+        {
+            Debug.Log("Player is in range.");
+        }
+
+        if (playerInRange && !dialogueActive && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Starting dialogue...");
+
+            dialogue.text = currentDialogue;
+            dialogue_box.SetActive(true);
+            dialogueActive = true;
+
+            if (playerMovement != null)
+            {
+                playerMovement.enabled = false;
+                Debug.Log("Player movement disabled.");
+            }
+        }
+        else if (dialogueActive && Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("Ending dialogue...");
+
+            dialogue_box.SetActive(false);
+            dialogueActive = false;
+
+            if (playerMovement != null)
+            {
+                playerMovement.enabled = true;
+                Debug.Log("Player movement re-enabled.");
+            }
+        }
     }
-    private void OnTriggerExit2D(Collider2D other) {
-        dialogue_box.SetActive(false);
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerEnter2D with: " + other.gameObject.name);
+
+        if (other.gameObject == Player)
+        {
+            Debug.Log("Player entered dialogue trigger.");
+            playerInRange = true;
+
+            switch (gameObject.tag)
+            {
+                case "Milkman":
+                    currentDialogue = "I am the milkman...";
+                    break;
+
+                case "Cow":
+                    currentDialogue = "Moo! (You can't milk the cow without a bucket)";
+                    break;
+
+                default:
+                    currentDialogue = "";
+                    break;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("OnTriggerExit2D with: " + other.gameObject.name);
+
+        if (other.gameObject == Player)
+        {
+            Debug.Log("Player exited dialogue trigger.");
+            playerInRange = false;
+
+            if (dialogueActive)
+            {
+                Debug.Log("Closing dialogue box due to player exit.");
+                dialogue_box.SetActive(false);
+                dialogueActive = false;
+
+                if (playerMovement != null)
+                {
+                    playerMovement.enabled = true;
+                    Debug.Log("Player movement re-enabled.");
+                }
+            }
+        }
     }
 }
