963e44643fb507dfa619be843ceccedbd77dc46d
IanMancuso <Ian.mancuso@gmail.com>
2025-04-17 10:11:00 -0400

    Click through and Pausing

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -12,9 +12,13 @@ public class Dialogue : MonoBehaviour
 
     public string item = "none";
     public string zone = "none";
+    public string[] dialogue_lines;
+    public int dialogue_num = 0;
 
     public bool milkman_complete = false;
     public bool cow_complete = false;
+    public bool paused = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -30,43 +34,68 @@ public class Dialogue : MonoBehaviour
             switch (zone){
                 case "Bucket":
                     item = "bucket";
-                    dialogue.text = "(You have picked up the bucket)";
+                    dialogue_lines = new string[1] { "(You have picked up the bucket)" };              
                     break;
                 
                 case "Cow":
-                if (!cow_complete) 
-                {
-                    if ( item == "bucket" )
+                    if (!cow_complete)
                     {
-                        cow_complete = true;
-                        item = "milk";
-                    } else {
-                        dialogue.text = "(You can't milk the cow without a bucket)";
+                        if (item == "bucket")
+                        {
+                            cow_complete = true;
+                            item = "milk";
+                        }
+                        else
+                        {
+                            dialogue_lines = new string[1] { "(You can't milk the cow without a bucket)" };
+                        }
                     }
-                }
-                if(cow_complete) {
-                    dialogue.text = "Moo! (You have milked the cow)";
-                }
+                    if (cow_complete)
+                    {
+                        dialogue_lines = new string[1] { "Moo! (You have milked the cow)" };
+                    }
+                    
                     break;
 
                 case "Milkman":
                     if (!milkman_complete)
                     {
-                        if (item == "milk"){
+                        if (item == "milk")
+                        {
                             milkman_complete = true;
-                        } else {
-                            dialogue.text = "I am the milkman. MORE TEXT. MORE TEXT. There is so much text in here to test the text boxes so there will be LOTS OF TEXT BIIIIIG TEXT yes so much text so much text yes yes yes yes yes yes yes I am the milk man";
+                        }
+                        else
+                        {
+                            dialogue_lines = new string[2] {"I am the milk man", "bring me milk, bug"};
                         }
                     }
                     if (milkman_complete)
                     {
-                        dialogue.text = "Thank you. I now have milk. You may proceed.";
-                    }
-                    break;
+                        dialogue_lines = new string[3] {"Thank you", "I now have milk.", "You may proceed." };
+                    }                 
+                break;
                 
                 default:
                     Debug.Log("Nothing to interact with");
                     break;
+            }
+            if (!paused)
+            {
+                PauseGame();
+            }
+            if(paused)
+            {
+                if(dialogue_num == dialogue_lines.Length)
+                {
+                    PauseGame();
+                    dialogue_num = 0;
+                } else {
+                    dialogue.text = dialogue_lines[dialogue_num];
+                    if (dialogue_num < dialogue_lines.Length)
+                    {
+                        dialogue_num++;
+                    }   
+                }   
             }
         }
     }
@@ -84,4 +113,18 @@ public class Dialogue : MonoBehaviour
         zone = "none";
     }
 
+    public void PauseGame()
+    {
+        if (paused)
+        {
+            Time.timeScale = 1;
+            paused = false;
+            dialogue_box.SetActive(false);
+            zone = "none";
+        } else
+        {
+            Time.timeScale = 0;
+            paused = true;
+        }
+    }
 }
