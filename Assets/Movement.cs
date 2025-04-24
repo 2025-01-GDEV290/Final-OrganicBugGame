cb5524d70fdfa6140fb25c09865e4e1c2e27e40a
Gabriel-2003 <113034401+Gabriel-2003@users.noreply.github.com>
2025-04-24 15:00:20 -0400

    section 2 blockout

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -6,6 +6,7 @@ public class Movement : MonoBehaviour
 {
     public float moveSpeed = 7f;
     public float jumpForce = 6f;
+    public float jumplock = 0.01f;
     private Vector3 flip;
 
     private Rigidbody2D rb;
@@ -22,7 +23,7 @@ public class Movement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
 
         // Basic jump check — jump only when vertical velocity is near zero
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < jumplock)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
