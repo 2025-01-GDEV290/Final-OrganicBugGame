d3ec35788bc647e34c86ad859b9518c509d87f28
IanMancuso <Ian.mancuso@gmail.com>
2025-05-06 14:43:57 -0400

    Revert "Merge branch 'Gabe-Section2-dialogue' into Organic-Game---dev"
    This reverts commit bd4a61e40519f376c9b871e0bda632a5de0929e4, reversing
    changes made to 46313337341ef640b20747a4ad657a4b8c9d6e26.

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -8,10 +8,9 @@ public class Movement : MonoBehaviour
     public float jumpForce = 6f;
     public float jumplock = 0.01f;
     private Vector3 flip;
-    private Animator animator;
+
     private Rigidbody2D rb;
     private Vector2 movement;
-    private SpriteRenderer sr;
 
     public GameObject spawn;
 
@@ -20,41 +19,23 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        animator = GetComponent<Animator>();
-        sr = GetComponent<SpriteRenderer>();
         rb.freezeRotation = true;
         rb.position = spawn.transform.position;
     }
 
     void Update()
     {
-        if (!enabled) return;
-
         movement.x = Input.GetAxisRaw("Horizontal");
 
-        if (movement.x == 0) 
-        {
-            animator.SetInteger("movement", 0);
-        }
-        else
-        {
-            animator.SetInteger("movement", 1);
-        }
-
-        if (movement.x > 0)
-            sr.flipX = true;  // Facing right
-        else if (movement.x < 0)
-            sr.flipX = false;
-
+        // Basic jump check � jump only when vertical velocity is near zero
         if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < jumplock)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-            animator.SetInteger("movement", 2);
         }
 
-        if (rb.position.y < -10)
+        if(rb.position.y < -10)
         {
-            rb.position = spawn.transform.position;
+            rb.position = spawn.transform.position; //new Vector2(-6, 11);
         }
 
         if (rb.velocity.x > 0)
@@ -62,33 +43,27 @@ public class Movement : MonoBehaviour
             flip = transform.localScale;
             flip.x = -1;
             transform.localScale = flip;
-
-            if (!walking.isPlaying)
-            {
+            if(!walking.isPlaying) {
                 walking.Play();
             }
+            
         }
-        else if (rb.velocity.x < 0)
+         else if (rb.velocity.x < 0)
         {
             flip = transform.localScale;
             flip.x = 1;
             transform.localScale = flip;
-
-            if (!walking.isPlaying)
-            {
+            if(!walking.isPlaying) {
                 walking.Play();
             }
-        }
-        else
-        {
+        } else {
             walking.Pause();
         }
+
     }
 
     void FixedUpdate()
     {
-        if (!enabled) return;
-
         rb.velocity = new Vector2(movement.x * moveSpeed, rb.velocity.y);
     }
 }
